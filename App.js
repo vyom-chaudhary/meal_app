59212a71966305e90452a4e8142911752f48ade9
vyom-chaudhary <vyom@itservicesindia.com>
2021-01-18 17:31:44 +0530

    post

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
+
+import MealsNavigator from './navigation/MealsNavigator'
+
+const FetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoading, setFontLoading] = useState(false)
+
+  if (!fontLoading) {
+    return (<AppLoading startAsync={FetchFonts} onFinish={() => setFontLoading(true)} onError={() => console.log('font loading in error')}></AppLoading>)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text style={{ fontFamily: 'open-sans-bold' }}>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <MealsNavigator></MealsNavigator>
   );
 }
 
